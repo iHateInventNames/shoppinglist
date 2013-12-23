93141fd39523712bb4b362808494f2c09499a1a3
David Gonzalez <malmstein@gmail.com>
2013-12-23 21:33:44 +0100

    Moving Shopping List to Gradle, way to go yet

---
+++ ShoppingListGradle/oIShoppingList/build/source/buildConfig/debug/org/openintents/shopping/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.openintents.shopping;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "org.openintents.shopping";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10044;
+}

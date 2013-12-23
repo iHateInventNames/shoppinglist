3a17aa79f2e953d580196bf19ae04cd5fcf22e41
David Gonzalez <malmstein@gmail.com>
2013-12-23 18:13:44 +0100

    created sample card list

--- ShoppingListGlassWare/GlassWare/src/main/java/org/openintents/shopping/glassware/MainActivity.java
+++ ShoppingListGlassWare/GlassWare/src/main/java/org/openintents/shopping/glassware/MainActivity.java
@@ -1,27 +1,85 @@
 package org.openintents.shopping.glassware;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+
+import com.google.android.glass.app.Card;
+import com.google.android.glass.widget.CardScrollAdapter;
+import com.google.android.glass.widget.CardScrollView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
+
+    private List<Card> mCards;
+    private CardScrollView mCardScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        createCards();
 
-
+        mCardScrollView = new CardScrollView(this);
+        ExampleCardScrollAdapter adapter = new ExampleCardScrollAdapter();
+        mCardScrollView.setAdapter(adapter);
+        mCardScrollView.activate();
+        setContentView(mCardScrollView);
     }
 
+    private void createCards() {
+        mCards = new ArrayList<Card>();
 
+        Card card;
 
+        card = new Card(this);
+        card.setText("This card has a footer.");
+        card.setFootnote("I'm the footer!");
+        mCards.add(card);
+
+        card = new Card(this);
+        card.setText("This card has a puppy background image.");
+        card.setFootnote("How can you resist?");
+        card.addImage(R.drawable.ic_launcher);
+        mCards.add(card);
+
+        card = new Card(this);
+        card.setText("This card has a mosaic of puppies.");
+        card.setFootnote("Aren't they precious?");
+        card.addImage(R.drawable.ic_launcher);
+        card.addImage(R.drawable.ic_launcher);
+        card.addImage(R.drawable.ic_launcher);
+        mCards.add(card);
+    }
+
+    private class ExampleCardScrollAdapter extends CardScrollAdapter {
+        @Override
+        public int findIdPosition(Object id) {
+            return -1;
+        }
+
+        @Override
+        public int findItemPosition(Object item) {
+            return mCards.indexOf(item);
+        }
+
+        @Override
+        public int getCount() {
+            return mCards.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mCards.get(position);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return mCards.get(position).toView();
+        }
+
+    }
 }
